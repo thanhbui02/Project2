6bf28cb02f0d52bc5c2f927b32bb762ddc600708
Bùi Tuấn Thành <thanh.bt204851@sis.hust.edu.vn>
2023-06-20 19:03:09 +0700

    API

--- api.py
+++ api.py
@@ -3,14 +3,12 @@ import math
 import pandas as pd
 import tensorflow as tf
 from tensorflow.keras.models import load_model
-import os
-# Tính độ dài tên miền
+#Tính độ dài tên miền
 def calculate_length(text):
     if not text:
         return 0
     return len(text)
-
-# Tính độ bất định (entropy) của tên miền
+#Tính độ bất định ( entropy) của tên miền
 def calculate_entropy(text):
     if not text:
         return 0
@@ -20,36 +18,28 @@ def calculate_entropy(text):
         if p_x > 0:
             entropy += -p_x * math.log(p_x, 2)
     return entropy
-
 # Tải lại mô hình từ tệp H5
 model = load_model('model.h5')
-
 # Khởi tạo ứng dụng Flask
 app = Flask(__name__)
-
 # Định nghĩa một route và phương thức xử lý
 @app.route('/api', methods=['POST'])
 def api():
+    entropy_train_vals = []
+    length_train_vals = []
+    threshold = 0.5
     # Lấy dữ liệu gửi từ client
     data = request.get_json()
-    
-    entropy_train_vals = calculate_entropy(data['domain'])
-    length_train_vals = calculate_length(data['domain'])
-    
-    predict_data = pd.DataFrame({'entropy': [entropy_train_vals], 'length': [length_train_vals]})
-    
+    entropy_train_vals=calculate_entropy(data['domain'])
+    length_train_vals=calculate_length(data['domain'])
+    predict_data = pd.DataFrame({'entropy': entropy_train_vals, 'length': length_train_vals}, index=[0])
     predictions = model.predict(predict_data)
-    
-    threshold = 0.5
-    
     if predictions > threshold:
-        result = "This may be DNS tunneling!"
+        result="This may be DNS tunneling!"
+        return jsonify(result)
     else:
-        result = "This may not be DNS tunneling!"
-    
-    return jsonify(result)
-
-# Đoạn mã dưới đây sẽ chỉ được thực thi khi chạy trên Heroku
-if __name__ == '__main__':
-    # Chạy ứng dụng Flask trên cổng môi trường Heroku (thông qua biến môi trường PORT)
-    app.run(port=int(os.environ.get('PORT', 5000)))
+        result="This may not be DNS tunneling!"
+        return jsonify(result)
+# Chạy ứng dụng Flask trên cổng 5000
+if __name__ == 'api':
+    app.run(port=5000)
